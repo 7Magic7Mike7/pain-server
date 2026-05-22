80e1574f5384b89eb68361f8a20f36edb42424f9
Michael Artner <38492376+7Magic7Mike7@users.noreply.github.com>
2026-05-22 12:57:30 +0200

    Use Central DB Config (#2)
    * loading db config from file
    
    * changed DB defaults from x/y to lat/lng

--- scripts/config.ts
+++ scripts/config.ts
@@ -1,10 +1,12 @@
 
 export class DbConfig {
-    static readonly TABLE_NAME = 'DummyPain';
-    static readonly TABLE_COLUMN_ID = 'id'
-    static readonly TABLE_COLUMN_LAT = 'x';
-    static readonly TABLE_COLUMN_LNG = 'y';
-    static readonly TABLE_COLUMN_VALUE = 'value';
-    static readonly TABLE_COLUMN_DATATYPE = 'datatype';
-    static readonly TABLE_COLUMN_PAINORIGIN = 'painorigin';
+    static readonly TABLE_NAME = process.env.TABLE_NAME || 'DummyPain';
+    static readonly TABLE_COLUMN_ID = process.env.COL_ID || 'id';
+    static readonly TABLE_COLUMN_LAT = process.env.COL_LAT || 'lat';
+    static readonly TABLE_COLUMN_LNG = process.env.COL_LNG || 'lng';
+    static readonly TABLE_COLUMN_VALUE = process.env.COL_VALUE || 'value';
+    static readonly TABLE_COLUMN_DATATYPE = process.env.COL_DATATYPE || 'datatype';
+    static readonly TABLE_COLUMN_PAINORIGIN = process.env.COL_PAINORIGIN || 'painorigin';
 }
+
+console.log(`Using DB config: TABLE_NAME=${DbConfig.TABLE_NAME}, ID_COL=${DbConfig.TABLE_COLUMN_ID}, LAT_COL=${DbConfig.TABLE_COLUMN_LAT}, LNG_COL=${DbConfig.TABLE_COLUMN_LNG}, VALUE_COL=${DbConfig.TABLE_COLUMN_VALUE}, DATATYPE_COL=${DbConfig.TABLE_COLUMN_DATATYPE}, PAINORIGIN_COL=${DbConfig.TABLE_COLUMN_PAINORIGIN}`);
