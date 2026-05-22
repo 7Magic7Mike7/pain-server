671216c9438c2e18c5c11825a7e98d61516ca7dc
Michael Artner <michael.artner@hotmail.de>
2026-05-22 12:53:16 +0200

    changed DB defaults from x/y to lat/lng

--- scripts/config.ts
+++ scripts/config.ts
@@ -2,8 +2,8 @@
 export class DbConfig {
     static readonly TABLE_NAME = process.env.TABLE_NAME || 'DummyPain';
     static readonly TABLE_COLUMN_ID = process.env.COL_ID || 'id';
-    static readonly TABLE_COLUMN_LAT = process.env.COL_LAT || 'x';
-    static readonly TABLE_COLUMN_LNG = process.env.COL_LNG || 'y';
+    static readonly TABLE_COLUMN_LAT = process.env.COL_LAT || 'lat';
+    static readonly TABLE_COLUMN_LNG = process.env.COL_LNG || 'lng';
     static readonly TABLE_COLUMN_VALUE = process.env.COL_VALUE || 'value';
     static readonly TABLE_COLUMN_DATATYPE = process.env.COL_DATATYPE || 'datatype';
     static readonly TABLE_COLUMN_PAINORIGIN = process.env.COL_PAINORIGIN || 'painorigin';
