af99993e4fbbc06b5a9e085e942039b54453c235
Michael Artner <38492376+7Magic7Mike7@users.noreply.github.com>
2026-09-03 23:29:24 +0200

    implemented database version for computing coordinates from survey input (#9)
    * implemented first version of computing coordinates from survey input
    
    * cleanup
    
    * implemented country to coordinate mapping
    
    * fixed bug in extracting the closest datapoint

---
+++ test/countryCoordinates.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect } from "vitest";
+import { countryToCoordinate } from "../src/config/country-coordinate-config";
+
+describe("Test countryToCoordinate()", () => {
+  describe("valid", () => {
+    it("AUT", () => {
+      const res = countryToCoordinate("AUT");
+      expect(res).toBeDefined();
+      expect(res?.lat).toBeCloseTo(48.2, 2);
+      expect(res?.lng).toBeCloseTo(16.37, 2);
+    });
+    it("GRC", () => {
+      const res = countryToCoordinate("GRC");
+      expect(res).toBeDefined();
+      expect(res?.lat).toBeCloseTo(37.98, 2);
+      expect(res?.lng).toBeCloseTo(23.73, 2);
+    });
+  });
+  describe("invalid", () => {
+    it("wrong country code", () => {
+      const res = countryToCoordinate("AT");
+      expect(res).toBeUndefined();
+    });
+  });
+});
